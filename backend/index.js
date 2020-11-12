443965735ab384591d287c9978d78740fff57ae0
Miguel Ángel Ramírez <miguelangel.ramirezurrego@iefaz.edu.co>
2020-11-12 14:51:07 +0000

    Estructura de backend y frontend

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const express= require('express');
 const app = express();
 const cultivo= require('./routes/cultivo');
 const planta= require('./routes/planta');
+const planta= require('./routes/propietarios');
+const planta= require('./routes/sensor');
 
 //ajustes
 app.set('port',3000)
@@ -15,8 +17,8 @@ app.use(express.json());
 // ajustess
 app.use('/api/cultivo',cultivo);
 app.use('/api/planta',planta);
-
-
+app.use('/api/propietarios',propietarios);
+app.use('/api/sensor',sensor);
 
 app.listen(app.get('port'),()=>{
     console.log(`Servidor corriendo en puerto  ${app.get('port')}`)
